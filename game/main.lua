5d374cb3e3530dfc0a5826372105126686c316a5
Christian James Lee <christianvehicle@gmail.com>
2025-10-23 12:20:28 -0400

    oshashit

--- game/main.lua
+++ game/main.lua
@@ -1,3 +1,10 @@
+function love.load()
+   image = love.graphics.newImage("Images/501.png")
+   love.graphics.setNewFont(12)
+   love.graphics.setColor(0,0,0)
+   love.graphics.setBackgroundColor(255,255,255)
+end
+
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+    love.graphics.draw(image, 100,100)
 end
