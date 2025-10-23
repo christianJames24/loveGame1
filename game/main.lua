d7a3e9b7ce5e63f9a03fd4038c4aff9d47909017
Christian James Lee <christianvehicle@gmail.com>
2025-10-23 12:34:08 -0400

    Update main.lua

--- game/main.lua
+++ game/main.lua
@@ -5,6 +5,24 @@ function love.load()
    love.graphics.setBackgroundColor(255,255,255)
 end
 
+xWalk = 0
+yWalk = 0
+function love.update(dt)
+   if love.keyboard.isDown("s") then
+    yWalk = yWalk + 2
+   end
+   if love.keyboard.isDown("w") then
+    yWalk = yWalk - 2
+   end
+   if love.keyboard.isDown("d") then
+    xWalk = xWalk + 2
+   end
+   if love.keyboard.isDown("a") then
+    xWalk = xWalk - 2
+   end
+end
+
 function love.draw()
-    love.graphics.draw(image, 100,100)
+   love.graphics.draw(image, xWalk, yWalk)
+   love.graphics.print("Click and drag piplup around or use the arrow keys", 10, 10)
 end
