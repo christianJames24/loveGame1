90b41abb00543b26cd67980d8c5178d318ee7e13
Christian James Lee <christianvehicle@gmail.com>
2025-10-29 12:55:02 -0400

    fun

--- game/main.lua
+++ game/main.lua
@@ -8,23 +8,33 @@ end
 xWalk = 0
 yWalk = 0
 function love.update(dt)
-   if love.keyboard.isDown("s") or love.keyboard.isDown("down") then
-    yWalk = yWalk + 2
-   end
-   if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
-    yWalk = yWalk - 2
-   end
-   if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
-    xWalk = xWalk + 2
-   end
-   if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
-    xWalk = xWalk - 2
-   end
+   -- if love.keyboard.isDown("s") or love.keyboard.isDown("down") then
+   --  yWalk = yWalk + 2
+   -- end
+   -- if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
+   --  yWalk = yWalk - 2
+   -- end
+   -- if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
+   --  xWalk = xWalk + 2
+   -- end
+   -- if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
+   --  xWalk = xWalk - 2
+   -- end
+
+   penis = love.mouse.getPosition( )
+   piplupX = love.mouse.getX()
+   piplupY = love.mouse.getY()
+   piplupxd = tostring(piplupX)..", "..tostring(piplupY)
+
+   xWalk = piplupX
+   yWalk = piplupY
 end
 
 function love.draw()
    love.graphics.draw(image, xWalk, yWalk, 0, 0.5)
-   love.graphics.print("Click and drag piplup around or use the arrow keys", 10, 10)
+   -- love.graphics.print("Click and drag oshashit around or use the arrow keys", 10, 10)
+   -- love.graphics.print(love.mouse.getPosition(),10,10)
+   love.graphics.print(piplupxd, 10,10)
 end
 
 function love.conf(t)
